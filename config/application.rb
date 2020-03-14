898694a61a9b72dc648d8f6dcfee799d22fd3914
sepuckett86 <sepuckett86@gmail.com>
2020-03-14 15:46:14 -0700

    Add jsonapi-resources gem

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,12 @@ module RailsApiCodeNotes
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    # rack-cors starting point. Update as needed.
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
   end
 end
